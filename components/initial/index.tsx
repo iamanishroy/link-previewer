b685abf7a24bbbbe569b34617530cfb7e140e1d4
Anish Roy <6275anishroy@gmail.com>
2022-08-02 23:50:23 +0530

    worked on initial page

---
+++ components/initial/index.tsx
@@ -0,0 +1,22 @@
+import commonStyles from '../../styles/Common.module.scss'
+import styles from './Styles.module.scss'
+
+export default function Initial() {
+    return (
+        <div className={commonStyles.container}>
+            <main className={commonStyles.main}>
+                <h1 className={styles.title}>
+                    Link Previewer
+                </h1>
+                <div className={styles.hrLine} />
+                <div className={styles.inputWrapper}>
+                    <label className={styles.label} >Enter your link here</label>
+                    <input className={styles.input} type='text' placeholder='https://google.com' />
+                    <p className={styles.info}>This link is not valid</p>
+                </div>
+                <button className={styles.button}>view</button>
+            </main>
+        </div>
+    )
+}
+
